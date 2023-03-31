e3b8438c4c59fd13aad9c90df03e24e2899448d3
Moris Sirotić <moris.sirotic1@gmail.com>
2023-04-01 01:38:30 +0200

    Db connection established

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
+import { useEffect, useState } from "react";
 import { CardProduct } from "./components/CardProduct";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { Top } from "./components/Top";
+import axios from "axios";
 
+type Props = {
+  message: string;
+};
 export const App = () => {
+ 
+
+  const [message, setMessage] = useState<Props>({ message: "kmkmkm" });
+
+  useEffect(() => {
+
+    axios.get<Props>("http://localhost:8000/api").then((val) => {
+
+    console.log(val);
+    
+      setMessage(val.data);
+    });
+  }, []);
+
   return (
     <div className="flex flex-col w-full h-full min-h-screen bg-neutral-100">
       <Navbar />
 
-<Top/>
+      <Top />
 
-      
+      <Footer />
 
-      <Footer/>
+      {message.message}
     </div>
   );
 };
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,43 @@
 import express from "express";
 import { config } from "dotenv";
 
+import knex from "knex";
+
 config();
 
 console.log();
 const app = express();
-const port = process.env.PORT;
+
+const { DB_USER, DB_PASSWORD, DB_PORT, DB_HOST, DB_NAME, DB_CLIENT, PORT } =
+  process.env;
+
+const db = knex({
+  client: DB_CLIENT,
+  connection: {
+    host: DB_HOST,
+    user: DB_USER,
+    port: DB_PORT,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+  },
+});
+
+app.use((req, res, next) => {
+  const allowedOrigins = ["http://localhost:5173", "http://localhost:8000"]; // Update with your frontend's origin
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server ssmss");
@@ -15,6 +47,12 @@ app.get("/api", (req, res) => {
   res.send({ message: "Hello, I work!" });
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.get("/api/users", async (req, res) => {
+  const ar = await db.select("name").from("users");
+
+  res.send(ar);
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
