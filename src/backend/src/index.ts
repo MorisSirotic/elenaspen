d490e1e5257524668417c2c1adc7b749aeef691e
Moris Sirotić <moris.sirotic1@gmail.com>
2023-04-02 01:17:00 +0200

    Everything works as it should

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,12 +1,45 @@
 import express from "express";
+import dotenv from "dotenv";
+import db from "./db";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const { DB_USER, DB_PASSWORD, DB_PORT, DB_HOST, DB_NAME, DB_CLIENT, PORT } =
+  process.env;
+
+app.use((req, res, next) => {
+  const allowedOrigins = ["http://localhost:5173", "http://localhost:8000"]; // Update with your frontend's origin
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(String(origin))) {
+    res.setHeader("Access-Control-Allow-Origin", String(origin));
+  }
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server sssmssd");
+});
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Hello, I work!" });
+});
+
+app.get("/api/users", async (req, res) => {
+  const ar = (await db.select("*").from("users")).at(0);
+
+  res.send(ar);
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is runninssssgs at http://localhost:${PORT}`);
 });
