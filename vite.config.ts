19efa0dc74aa0c36c7385b03bcfcbeffe8775e02
Moris Sirotić <moris.sirotic1@gmail.com>
2023-04-19 02:01:34 +0200

    HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/elenaspen.com/privkey.pem",
+        "utf8"
+      ), // Path to your private key file
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/elenaspen.com/fullchain.pem",
+        "utf8"
+      ), // Path to your SSL certificate file
+    },
+  },
+});
